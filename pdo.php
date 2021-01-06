68265a533506dbb7dbd77bf7afede19e9663016b
Nirodha Lakmali <58288134+Nirodha-Github@users.noreply.github.com>
2021-01-06 22:46:33 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,7 +1,7 @@
 <?php
 
 # For windows = 3306
-# Use db name instead of Database_name
+# Use db name instead of Learn_Crud
 
 try {
 	$pdo = new PDO('mysql:host=localhost;port=3306;dbname=Learn_Crud', 
