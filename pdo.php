30eaf4887537f1fa8b94b6a44f5765b372e17899
Nirodha Lakmali <58288134+Nirodha-Github@users.noreply.github.com>
2021-01-06 22:45:42 +0530

    Update and rename configuration.php to pdo.php

--- pdo.php
+++ pdo.php
@@ -4,7 +4,7 @@
 # Use db name instead of Database_name
 
 try {
-	$pdo = new PDO('mysql:host=localhost;port=3306;dbname=Database_name', 
+	$pdo = new PDO('mysql:host=localhost;port=3306;dbname=Learn_Crud', 
    'root', '');
 } 
 catch (Exception $e) {
